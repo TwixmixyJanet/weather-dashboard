fcbeb277c799907c2089c17208d3637526d527b7
Janet Webster <twixmixy@gmail.com>
2023-10-06 09:38:34 -0400

    Made comments/pseudocode & obtained API

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//     Weather App
+
+// API - connect to Weather and Geolocation
+    // HINT: need to register for an API key for the geolocation for lat and long
+    // Need to register for weather API key - could take 2hrs to activate
+    var weatherAPIKey = "b2da1db112f5ef2d52f8606a017dc730";
+    // FETCH API url
+    // give it a variable
+    // parse data
+
+// Query search
+    // Add city based off search
+    // Create new button for search
+    // limit to top 10
+    // prevent adding same city multiple times, if possible
+    // Only allow a button to be added if it’s a valid search/location
+
+// Local Storage
+    // Search history is what persists
+
+// Today's Weather
+    // City / Date / Emoji /  Temp / Wind / Humidity
+
+// 5-Day Forecase
+    // 5-day forecast begins at tomorrow
+    // Date / Emoji / Temp / Wind / Humidity
+
+
+

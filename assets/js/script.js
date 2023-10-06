9f2d42d80f39c4a6b7662fa289fa99b14ea31e47
Janet Webster <twixmixy@gmail.com>
2023-10-06 11:18:43 -0400

    Drafting out functions to display weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
     // HINT: need to register for an API key for the geolocation for lat and long
     // Need to register for weather API key - could take 2hrs to activate
 var weatherAPIKey = "b2da1db112f5ef2d52f8606a017dc730";
-var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${weatherAPIKey}`;
+var weatherAPIURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${weatherAPIKey}`;
     // FETCH API url
     // give it a variable
     // parse data
@@ -26,6 +26,48 @@ function dashboard(e) {
     e.preventDefault();
     var cityName = cityInputElement.ariaValueMax;
     displayWeather(cityName);
+}
+
+function displayWeather(cityName) {
+    console.log(cityName);
+    fetch(weatherAPIURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (currentData) {
+            console.log(currentData);
+            var currentCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${weatherAPIKey}&units=imperial`;
+            fetch(currentCallURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then (function (fiveDayData) {
+                    if (searchHistory.includes(currentData.name) === false) {
+                        searchHistory.push(currentData.name);
+                        localStorage.setItem("city", JSON.stringify(searchHistory));
+                    }
+                    displayCity();
+                    console.log(fiveDayData);
+                    todaysWeather.innerHTML = 
+                    `
+                    <ul>
+                    <li class="title">${currentData.name} ||<span> ${dayjs(currentData.dt, "X").format(" dddd, MMM D YYYY retrieved at h:mm:ssA")} </span></li>
+                    <li><img src="https://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png" /></li>
+                    <li>Temperature: ${currentData.main.temp}</li>
+                    <li>Wind: ${currentData.wind.speed}</li>
+                    <li>Humidity: ${currentData.main.humidity}</li>
+                    <li>UV Index: ${currentData.current.uvi}</li>
+                    </ul>
+                    `;
+                    var days = "";
+                    for (var i = 1; i < 6; i++) {
+                        days = days +
+                        `
+                        
+                        `
+                    }
+                })
+        })
 }
 
 // Local Storage
